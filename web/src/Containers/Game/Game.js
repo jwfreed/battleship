8e9da0fed3ec69169d7f038dc0811a2767de6fc0
Jonathan Freed <jon.freed@gmail.com>
2020-08-01 13:04:08 -0400

    Update Game.js

--- web/src/Containers/Game/Game.js
+++ web/src/Containers/Game/Game.js
@@ -16,6 +16,7 @@ const Game = () => {
   return (
     <div className="game game-container">
       <header className="title">BATTLESHIP</header>
+      <p>Hi, I'm Jon Freed. Repo: <a href="https://github.com/jwfreed/battleship">github.com/jwfreed/battleship</a> </p>
       <GameContext.Provider value={{ ...state, dispatch }}>
         {state.matchID ? <Match /> : <CreateOrJoinGame />}
       </GameContext.Provider>
