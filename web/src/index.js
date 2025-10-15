6655f499644b27207949bed35298dbf53bd07208
Jonathan Freed <jon.freed@gmail.com>
2025-10-14 23:43:57 -0400

    better design

--- web/src/index.js
+++ web/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import Game from './Containers/Game/Game';
+import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 root.render(<Game />);
